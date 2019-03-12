72440baaa4b212de7a28ba2193458082bf0a0b6b
henrygambles <gambles.henry2@gmail.com>
2019-03-12 15:06:36 +0000

    End of step 8 (touch_in & touch_out support added.)

---
+++ codeformat.rb
@@ -0,0 +1,51 @@
+require 'oystercard'
+
+describe Oystercard do
+    
+    subject(:card) {Oystercard.new}
+    describe '#Balance' do
+        
+        it 'default balance is zero' do
+            expect(card.balance).to eq Oystercard::START_BALANCE
+        end
+    end
+    
+    describe '#Top_up' do
+        
+        it 'Responds to top_up method' do
+            expect(card).to respond_to(:top_up)
+        end
+        
+        it 'Balance should change when topped up' do
+            expect(card.top_up(5)).to eq(5)
+            #expect {top_up}.to change {balance}.by(5)
+        end
+    end
+    
+    it 'Error msg raised when max balance reached' do
+        expect{ card.top_up(100) }.to raise_error('Max balance reached')
+    end
+    
+    describe '#deduct' do
+        it 'responds to deduct method' do
+            expect(card).to respond_to(:deduct)
+        end
+        
+        it 'balance should change when deducted' do
+            expect(card.deduct(2)).to eq(-2)
+        end
+    end
+    
+    
+    describe '#journey' do
+        it 'allows the customer to touch in and pass through the barriers' do
+        expect(card).to respond_to(:touch_in)
+    end
+end
+
+end
+
+Message Input
+
+
+Message Harry Galliano
